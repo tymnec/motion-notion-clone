7b45c10e2f6bf133b3334ca037ca7f15162dfa84
Nikhil <tymnec@gmail.com>
2023-12-20 12:18:15 +0530

    documentation: menu added

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -1,42 +1,65 @@
 "use client";
 
+// Import necessary modules and components
+
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
 import { toast } from "sonner";
 import { MoreHorizontal, Trash } from "lucide-react";
-
 import { Id } from "@/convex/_generated/dataModel";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 
+/**
+ * Interface for the Menu component props.
+ */
 interface MenuProps {
+  /**
+   * The ID of the document.
+   */
   documentId: Id<"documents">;
-};
+}
 
-export const Menu = ({
-  documentId
-}: MenuProps) => {
-  const router = useRouter();
-  const { user } = useUser();
-
+/**
+ * Renders a menu component for a document.
+ *
+ * @param {MenuProps} props - The properties for the menu.
+ * @return {JSX.Element} The rendered menu component.
+ */
+export const Menu = ({ documentId }: MenuProps) => {
+  /**
+   * Function to handle archiving of documents
+   */
   const archive = useMutation(api.documents.archive);
 
+  // Get the router instance
+  const router = useRouter();
+
+  // Get the user from useUser hook
+  const { user } = useUser();
+
+  /**
+   * Executes the onArchive function.
+   *
+   * @returns {void} - This function does not return anything.
+   */
   const onArchive = () => {
-    const promise = archive({ id: documentId })
+    // Call the archive function with the `id` parameter
+    const promise = archive({ id: documentId });
 
     toast.promise(promise, {
       loading: "Moving to trash...",
       success: "Note moved to trash!",
-      error: "Failed to archive note."
+      error: "Failed to archive note.",
     });
 
     router.push("/documents");
@@ -49,10 +72,10 @@ export const Menu = ({
           <MoreHorizontal className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent 
-        className="w-60" 
-        align="end" 
-        alignOffset={8} 
+      <DropdownMenuContent
+        className="w-60"
+        align="end"
+        alignOffset={8}
         forceMount
       >
         <DropdownMenuItem onClick={onArchive}>
@@ -68,8 +91,11 @@ export const Menu = ({
   );
 };
 
+/**
+ * Renders a skeleton for the menu.
+ *
+ * @return {JSX.Element} The rendered skeleton component.
+ */
 Menu.Skeleton = function MenuSkeleton() {
-  return (
-    <Skeleton className="h-10 w-10" />
-  )
-}
+  return <Skeleton className="h-10 w-10" />;
+};
