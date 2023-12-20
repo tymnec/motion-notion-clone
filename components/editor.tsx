520c22173e48800d49248577d8f566d1e30df21a
Nikhil <tymnec@gmail.com>
2023-12-20 11:15:08 +0530

    testing phase 1

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import {
-  BlockNoteEditor,
-  PartialBlock
-} from "@blocknote/core";
-import {
-  BlockNoteView,
-  useBlockNote
-} from "@blocknote/react";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
 
 import { useEdgeStore } from "@/lib/edgestore";
@@ -17,35 +11,28 @@ interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
   editable?: boolean;
-};
+}
 
-const Editor = ({
-  onChange,
-  initialContent,
-  editable
-}: EditorProps) => {
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
   const { edgestore } = useEdgeStore();
 
   const handleUpload = async (file: File) => {
-    const response = await edgestore.publicFiles.upload({ 
-      file
+    const response = await edgestore.publicFiles.upload({
+      file,
     });
 
     return response.url;
-  }
+  };
 
   const editor: BlockNoteEditor = useBlockNote({
     editable,
-    initialContent: 
-      initialContent 
-      ? JSON.parse(initialContent) as PartialBlock[] 
-      : undefined,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
-    uploadFile: handleUpload
-  })
+    uploadFile: handleUpload,
+  });
 
   return (
     <div>
@@ -54,7 +41,7 @@ const Editor = ({
         theme={resolvedTheme === "dark" ? "dark" : "light"}
       />
     </div>
-  )
-}
+  );
+};
 
 export default Editor;
