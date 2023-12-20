e627e34821e828dcc5ce6395ae272f3dfeefe2ed
Nikhil <tymnec@gmail.com>
2023-12-20 12:32:25 +0530

    documentation: confirm modal added

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -17,10 +17,24 @@ interface ConfirmModalProps {
   onConfirm: () => void;
 };
 
+/**
+ * Renders a confirm modal component.
+ *
+ * @param {ConfirmModalProps} props - The properties for the confirm modal.
+ * @param {ReactNode} props.children - The content of the confirm modal.
+ * @param {() => void} props.onConfirm - The callback function to be called when the confirm button is clicked.
+ * @return {JSX.Element} The JSX element representing the confirm modal.
+ */
 export const ConfirmModal = ({
   children,
   onConfirm
 }: ConfirmModalProps) => {
+  /**
+   * Handles the confirm event.
+   *
+   * @param {React.MouseEvent<HTMLButtonElement, MouseEvent>} e - The event object.
+   * @return {void} No return value.
+   */
   const handleConfirm = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
